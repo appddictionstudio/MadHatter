b8a87f89d0ea026722d44e6b639bc50d4685a912
caseydrobinson <42978528+caseydrobinson@users.noreply.github.com>
2019-01-03 15:06:05 -0600

    routing to home page

--- MadHatter/src/app/app-routing.module.ts
+++ MadHatter/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  { path: 'login', component: LoginComponent }
+  { path: '', component: LoginComponent},
+  {path: '', children: [
+    {path: 'home', component: HomeComponent}
+  ]}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+ }
