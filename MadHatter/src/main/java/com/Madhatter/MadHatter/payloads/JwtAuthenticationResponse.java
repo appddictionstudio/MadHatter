16241ee5a4d299b9e2d15ddaf867e4e81edc764c
dcraig3 <45215486+dcraig3@users.noreply.github.com>
2019-01-04 15:16:59 -0600

    Payloads

---
+++ MadHatter/src/main/java/com/Madhatter/MadHatter/payloads/JwtAuthenticationResponse.java
@@ -0,0 +1,38 @@
+package com.Madhatter.MadHatter.payloads;
+
+public class JwtAuthenticationResponse {
+    private String tokenType = "Bearer ";
+    private String accessToken;
+
+    public JwtAuthenticationResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    /**
+     * @return the accessToken
+     */
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    /**
+     * @param accessToken the accessToken to set
+     */
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    /**
+     * @return the tokenType
+     */
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    /**
+     * @param tokenType the tokenType to set
+     */
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+}
