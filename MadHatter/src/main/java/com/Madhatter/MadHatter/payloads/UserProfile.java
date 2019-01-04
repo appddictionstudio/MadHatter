104f22253909ef7aa6d57e7003a68edd65938575
caseydrobinson <42978528+caseydrobinson@users.noreply.github.com>
2019-01-04 15:00:06 -0600

    adding security

---
+++ MadHatter/src/main/java/com/Madhatter/MadHatter/payloads/UserProfile.java
@@ -0,0 +1,88 @@
+package com.Madhatter.MadHatter.payloads;
+
+public class UserProfile {
+
+    private Long id;
+    private String name;
+    private String userName;
+    private String email;
+    private String role;
+
+    public UserProfile(Long id, String name, String userName, String email, String role) {
+        this.id = id;
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.role = role;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @param role the role to set
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ MadHatter/src/main/java/com/Madhatter/MadHatter/security/CurrentUser.java
@@ -0,0 +1,10 @@
+package com.Madhatter.MadHatter.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {}
