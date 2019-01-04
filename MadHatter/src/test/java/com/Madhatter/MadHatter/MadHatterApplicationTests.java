95020336c8b32a04f3efc82d3e6cacd313a0aa77
caseydrobinson <42978528+caseydrobinson@users.noreply.github.com>
2019-01-04 13:19:50 -0600

    Starting new Spring Project

---
+++ MadHatter/src/test/java/com/Madhatter/MadHatter/MadHatterApplicationTests.java
@@ -0,0 +1,17 @@
+package com.Madhatter.MadHatter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MadHatterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
