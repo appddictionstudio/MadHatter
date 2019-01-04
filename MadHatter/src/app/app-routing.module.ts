5ae95257e2e155244681f5c7a2e5203cf426168a
dcraig3 <45215486+dcraig3@users.noreply.github.com>
2019-01-04 09:20:36 -0600

    Linked the learning page

--- MadHatter/src/app/app-routing.module.ts
+++ MadHatter/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { LearningComponent } from './learning/learning.component';
 const routes: Routes = [
-  { path: '', component: LoginComponent},
-  {path: '', children: [
-    {path: 'home', component: HomeComponent}
+  { path: '', component: LoginComponent },
+  { path: '', children: [
+    { path: 'home', component: HomeComponent },
+    { path: `learning`, component: LearningComponent }
   ]}
 ];
 
