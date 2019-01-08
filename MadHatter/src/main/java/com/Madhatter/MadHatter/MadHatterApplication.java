c8d603b5581b9ea7f0b75ee0a1aa0019c75b9079
jaortega527 <aortega@appddictionstudio.com>
2019-01-07 19:03:11 -0600

    added tomcat plugin to java application

--- MadHatter/src/main/java/com/Madhatter/MadHatter/MadHatterApplication.java
+++ MadHatter/src/main/java/com/Madhatter/MadHatter/MadHatterApplication.java
@@ -2,12 +2,21 @@ package com.Madhatter.MadHatter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.hcde.hcde.HcdeApplication;
 
 @SpringBootApplication
-public class MadHatterApplication {
+public class MadHatterApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MadHatterApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HcdeApplication.class);
 	}
 
 }
