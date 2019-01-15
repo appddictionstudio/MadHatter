36f4e340f2481128290d4ce254d0db25caef3448
dcraig3 <45215486+dcraig3@users.noreply.github.com>
2019-01-15 15:44:47 -0600

    topic controller

--- MadHatter/src/main/java/com/Madhatter/MadHatter/controllers/TopicController.java
+++ MadHatter/src/main/java/com/Madhatter/MadHatter/controllers/TopicController.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,7 +30,7 @@ public class TopicController {
 	   
 	  //--------------- Load -----------------------------------------------------------
 	  	 @RequestMapping(method = RequestMethod.GET)
-	  	 ResponseEntity<List<Topic>> getAllMhTopics() {	
+	  	ResponseEntity<List<Topic>> getAllMhTopics() {	
 	  	    List<Topic> postList = repo.getAllTopics();	 
 	  	    return ResponseEntity.ok(postList);
 	  	    }
@@ -34,11 +38,20 @@ public class TopicController {
 	  //-----------------LoadByModId------------------------------------------------------
 	  	 
 	  	@RequestMapping(value = "/getByModId/{id}", method = RequestMethod.GET)
-		ResponseEntity<Optional<Modules>> getTopicByModId(@PathVariable long id) {
+	  	ResponseEntity<Optional<Modules>> getTopicByModId(@PathVariable long id) {
 	  		
 			Optional<Modules> topics = repo.findByModId(id);
 			
 			return ResponseEntity.ok(topics);
 		};
+		
+		//--------------- set hidden or shown -----------------------------------------------------------
+		@RequestMapping(value = "/hide", method = RequestMethod.POST)
+	  	ResponseEntity<Topic> postAllMhTopics(@RequestBody Topic topic) {	
+			System.out.println("sometexthere");
+	  		topic.setHidden(topic.getHidden());
+	  		repo.save(topic);
+	  		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(topic);
+	  	    }
 
 }
