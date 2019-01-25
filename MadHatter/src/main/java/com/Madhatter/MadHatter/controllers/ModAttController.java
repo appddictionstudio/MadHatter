a3a8f0b8148d0309948d59cf4fbe5e6614b91ef7
caseydrobinson <42978528+caseydrobinson@users.noreply.github.com>
2019-01-25 11:36:30 -0600

    Mod Att controller and repository

---
+++ MadHatter/src/main/java/com/Madhatter/MadHatter/controllers/ModAttController.java
@@ -0,0 +1,101 @@
+package com.Madhatter.MadHatter.controllers;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.ValidationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.Madhatter.MadHatter.Repositories.ModAttRepository;
+import com.Madhatter.MadHatter.Repositories.TopicAttRepository;
+import com.Madhatter.MadHatter.models.Attachment;
+import com.Madhatter.MadHatter.models.ModAtt;
+import com.Madhatter.MadHatter.models.SubmittedAtt;
+import com.Madhatter.MadHatter.models.Topic;
+import com.Madhatter.MadHatter.models.TopicAtt;
+import com.Madhatter.MadHatter.services.AttachmentService;
+
+
+
+
+@RestController
+public class ModAttController {
+	
+
+
+@Autowired
+private AttachmentService attService;
+
+@Autowired
+private ModAttRepository repo;
+
+	//----Upload--------
+	
+		@PostMapping(value="/uploadMDoc")
+	    public ResponseEntity<ModAtt> uploadFile(@RequestParam("file") MultipartFile file) {
+	        try {
+	       	 
+	       	 Attachment att = attService.storeFile(file);
+	       	 
+	       	 ModAtt modAtt = new ModAtt();
+
+	       	modAtt.setAttachmentId(att.getId());
+	       	modAtt.setFileNm(file.getOriginalFilename());
+	       	modAtt.setFileSz(new Long(att.getAttachment().length));
+	       	
+	            repo.save(modAtt);
+
+	            return ResponseEntity.ok(modAtt);
+	        } catch (Exception e) {
+	            throw e;
+	        }
+	    }
+		
+		//----------------Downloading----------
+		
+				@RequestMapping(value = "/ModAtt/downloadDoc/{id}", method = RequestMethod.GET)
+				@ResponseBody
+				public void getExcelTemplate(HttpServletRequest request, HttpServletResponse response,@PathVariable Long id) throws Exception {
+					
+					ModAtt attachment = repo.getOne(id);
+					Attachment att = attService.getFile(attachment.getAttachmentId());
+					response.setContentType("application/octet-stream");
+					response.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+				    response.setHeader("Content-Disposition", "attachment; filename=" + attachment.getFileNm());
+
+				    
+				    /** assume that below line gives you file content in byte array **/
+				    byte[] binary = att.getAttachment();
+				    
+//				    int x = fis.available();
+//				    byte byteArray[] = new byte[x];
+//				    logger.info(" File size :"+byteArray.length);
+//				    fis.read(byteArray);
+
+				    response.getOutputStream().write(binary);
+				    response.flushBuffer();
+//				    fis.close();
+				}
+		
+}
---
+++ MadHatter/src/main/java/com/Madhatter/MadHatter/models/ModAtt.java
@@ -0,0 +1,93 @@
+package com.Madhatter.MadHatter.models;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+@Entity
+@Table(name="MOD_ATT")
+
+public class ModAtt implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MOD_ATT_SEQUENCE")
+	private long id;
+	
+	@Column(name="ATTACHMENT_ID")
+	private long attachmentId;
+	
+	@ManyToOne
+	@JoinColumn(name="MOD_ID", referencedColumnName="ID", nullable=false)
+	private Modules mod;
+	 
+	@Column(name="FILE_NM")
+	private String fileNm;
+		
+	@Column(name="FILE_SZ")
+	private long fileSz;
+		
+	@Column(name="DESCRIPTION")
+	private String description;
+
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public long getAttachmentId() {
+		return attachmentId;
+	}
+
+	public void setAttachmentId(long attachmentId) {
+		this.attachmentId = attachmentId;
+	}
+
+
+	public Modules getMod() {
+		return mod;
+	}
+
+	public void setMod(Modules mod) {
+		this.mod = mod;
+	}
+
+	public String getFileNm() {
+		return fileNm;
+	}
+
+	public void setFileNm(String fileNm) {
+		this.fileNm = fileNm;
+	}
+
+	public long getFileSz() {
+		return fileSz;
+	}
+
+	public void setFileSz(long fileSz) {
+		this.fileSz = fileSz;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
