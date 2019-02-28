cb1adab4cf88ae72a5126a0065a7b13fe83ce37a
dcraig3 <45215486+dcraig3@users.noreply.github.com>
2019-02-28 13:28:13 -0600

    fixed quizzes

--- MadHatter/src/app/learning-details/learning-details.component.ts
+++ MadHatter/src/app/learning-details/learning-details.component.ts
@@ -169,7 +169,6 @@ export class LearningDetailsComponent implements OnInit, OnChanges {
   getTopicsByModId(modId) {
     this.apiT.getTopicsByModId(modId).subscribe(data => {
       this.topics =  data as any[];
-      console.log(data);
       // console.log(this.topics);
       this.isLoading = false;
     });
